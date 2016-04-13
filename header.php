a243d29d179fcc8ea30754bff2b77fd22fd4ea01
worldvini <worldvini@hotmail.com>
2016-04-13 18:29:28 -0300

    First step
    creating all essential files

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+	<title><?php wp_title(); ?></title>
+	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
+
+	<?php wp_head(); ?>
+</head>
+<body>
+
