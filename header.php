79dbc77cd56944c63e7aec371ab73729223ec4b5
worldvini <worldvini@hotmail.com>
2016-04-14 00:21:12 -0300

    creating box blog
    criado a parte blog, e inserido a pagina sobre automaticamente no index
    do WP.

--- header.php
+++ header.php
@@ -4,8 +4,33 @@
 <meta charset="UTF-8">
 	<title><?php wp_title(); ?></title>
 	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
 
 	<?php wp_head(); ?>
 </head>
 <body>
+
+<div class="header">
+	<div class="wrap">
+		<h1><a href="<?php echo site_url(); ?>" title="<?php bloginfo('name'); ?>">WORLDVINI</a></h1>
+		<div class="infor">
+
+			<?php 
+				$args = array('post_type' => 'page', 'pagename' => 'sobre');
+				$my_page = get_posts( $args );
+			 ?>
+			 <?php if($my_page) : foreach ($my_page as $post)  : setup_postdata( $post ); ?>
+
+			 	<h2><?php the_title(); ?></h2>
+			 	<?php the_excerpt(); ?>
+			 	<a href="<?php the_permalink(); ?>" class="custom-botao">Leia mais</a>
+
+			 <?php endforeach; ?>	
+			
+			 <?php else: ?>
+			 	<p>Nenhum conteudo inserido na pagina sobre.</p>
+			 <?php endif; ?>	
+		</div>
+	</div>
+</div>
 
--- index.php
+++ index.php
@@ -1,5 +1,33 @@
 <?php get_header(); ?>
 
-<h2>Teste</h2>
+<div class="blog">
+	<div class="wrap">
+		<h2>BLOG</h2>
+
+		<div class="box-list-posts">
+
+			<div class="list-post">
+				<img src="<?php bloginfo('template_directory'); ?>/assets/images/pic02.jpg" alt="">
+				<div class="box-content-post">
+					<h2>when an unknown printer took</h2>
+					<p>t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'</p>
+					<a href="#" class="custom-botao">Leia mais</a>
+				</div>
+			</div>
+
+			<div class="list-post segundo-post">
+				
+				<div class="box-content-post">
+					<h2>when an unknown printer took</h2>
+					<p>t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'</p>
+					<a href="#" class="custom-botao">Leia mais</a>
+				</div>
+
+				<img src="<?php bloginfo('template_directory'); ?>/assets/images/pic02.jpg" alt="">
+			</div>
+		</div>
+
+	</div>
+</div>
 
 <?php get_footer(); ?>
