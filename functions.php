f6e637a63972ae7b9f4b02690ca2de815fd06aed
worldvini <worldvini@hotmail.com>
2016-05-20 03:08:14 -0300

    registro de funções do WP e automação da index

---
+++ functions.php
@@ -0,0 +1,33 @@
+<?php 
+
+//habilitando imagens destacadas
+add_theme_support('post-thumbnails');
+add_image_size('thumb-custom', 200, 200, true);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
--- index.php
+++ index.php
@@ -5,29 +5,22 @@
 		<h2>BLOG</h2>
 
 		<div class="box-list-posts">
-
-			<div class="list-post">
-				<img src="<?php bloginfo('template_directory'); ?>/assets/images/pic02.jpg" alt="">
-				<div class="box-content-post">
-					<h2>when an unknown printer took</h2>
-					<p>t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'</p>
-					<a href="#" class="custom-botao">Leia mais</a>
+			<?php 
+				$args = array('post_type'=>'post', 'showposts'=> 2);
+				$my_posts = get_posts( $args );
+			?>
+			<?php  $cont = 1; if($my_posts) : foreach( $my_posts as $post) : setup_postdata( $post ); ?>
+				<div class="list-post <?php if( $cont == 2 ) echo"segundo-post"; ?>">
+					<?php the_post_thumbnail();//padrão do WP para puxar foto do post ?>
+					<div class="box-content-post">
+						<h2><?php the_title(); // padrão do WP para puxar title do post?></h2>
+						<p><?php the_excerpt(); //padrão do WP para puxar resumo do post ?>
+						<a href="<?php the_permalink(); //padrão do WP para criar link do post?>" class="custom-botao">Leia mais</a>
+					</div>
 				</div>
-			</div>
+			<?php $cont ++ ; endforeach; endif;  ?>
 
-			<div class="list-post segundo-post">
-				
-				<div class="box-content-post">
-					<h2>when an unknown printer took</h2>
-					<p>t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'</p>
-					<a href="#" class="custom-botao">Leia mais</a>
-				</div>
-
-				<img src="<?php bloginfo('template_directory'); ?>/assets/images/pic02.jpg" alt="">
-			</div>
 		</div>
-
-	</div>
 </div>
 
 <?php get_footer(); ?>
